f06cedd0a3c617320798abc6cc3e7088ede3e4b1
VAMSHI <94733980+VAMSHI525@users.noreply.github.com>
2026-02-11 12:07:25 +0530

    Uploading Config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+
+HF_API_TOKEN = "<HuggingFace_Read_Token>"
+HF_MODEL_NAME = "openai/gpt-oss-120b" #"Qwen/Qwen3-Coder-30B-A3B-Instruct0" #
+
+SQL_SERVER_CONN_STR = (
+    "DRIVER={ODBC Driver 17 for SQL Server};"
+    "SERVER=<server_anme>;"
+    "DATABASE=T20;" #SDQ
+    "UID=SDQ_Login;"
+    "PWD=<password>"
+)
